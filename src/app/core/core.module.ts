df81ba2ff0216915157bc657721cabccfbc1ac68
Esteban Castro <estebancastrosola@gmail.com>
2020-02-17 12:19:09 +0100

    first commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Services
+import { UserService } from '@core/services/user.service';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+
+  ],
+})
+export class CoreModule { }
